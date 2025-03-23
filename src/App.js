10d2229ae64b19e1b483eac329eb635403d4e769
IMBALANSE <razvalilistranu@yandex.ru>
2025-03-23 20:24:54 +0400

    feat: transfer Oxygen, ClientsApi, ConsultationForm in 'pages'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
-import Oxygen from './components/Oxygen/Oxygen';
+import Oxygen from './pages/Oxygen/Oxygen';
 import About from './components/About/About';
 import Service from './components/Service/Service';
 import Price from './components/Prices/Prices';
 import Clients from './components/Clients/Clients';
-import ClientsApi from './components/ClientsApi/ClientsApi'; 
+import ClientsApi from './pages/ClientsApi/ClientsApi'; 
 import Consultation from './components/Consultation/Consultation';
-import ConsultationForm from './components/ConsultationForm/ConsultationForm';
+import ConsultationForm from './pages/ConsultationForm/ConsultationForm';
 import Contacts from './components/Contacts/Contacts';
 import Footer from './components/Footer/Footer';
 
