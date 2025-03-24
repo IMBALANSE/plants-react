2051809614aa6bd7781f78e11543fe9745e7b2e6
IMBALANSE <razvalilistranu@yandex.ru>
2025-03-24 21:32:55 +0400

    feat: add local fonts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import * as ReactDOMClient from 'react-dom/client'; 
+import './styles/fonts.css';
 import './styles/global.css'; // вначале ставим глобальные стили, и только после подключения App все стили редактируются каждым компонентом
 import App from "./App";
 // import './styles/main.css';
